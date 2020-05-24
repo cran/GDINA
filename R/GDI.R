b69c7259953b535290ced5ec57dec61d1e8ce1ed
Wenchao Ma <wenchao.ma@ua.edu>
2020-05-24 14:20:12 +0000

    version 2.8.0

--- R/GDI.R
+++ R/GDI.R
@@ -1,6 +1,6 @@
 #' Q-matrix validation
 #'
-#' Q-matrix validation for the (sequential) G-DINA model based on PVAF (de la Torre & Chiu, 2016; Najera, Sorrel, & Abad, 2019), stepwise Wald test (Ma & de la Torre, 2019) or mesa plot (de la Torre & Ma, 2016).
+#' Q-matrix validation for the (sequential) G-DINA model based on PVAF (de la Torre & Chiu, 2016; Najera, Sorrel, & Abad, 2019), stepwise Wald test (Ma & de la Torre, 2020) or mesa plot (de la Torre & Ma, 2016).
 #' All these methods are suitable for dichotomous and ordinal response data. If too many modifications are suggested based on the default PVAF method, you are suggested to try the stepwise Wald test method or predicted cutoffs.
 #' You should always check the mesa plots for further examination.
 #'
@@ -35,7 +35,7 @@
 #'
 #' de la Torre, J., & Ma, W. (2016, August). Cognitive diagnosis modeling: A general framework approach and its implementation in R. A Short Course at the Fourth Conference on Statistical Methods in Psychometrics, Columbia University, New York.
 #'
-#' Ma, W. & de la Torre, J. (2019). An Empirical Q-Matrix Validation Method for the Sequential G-DINA Model. \emph{British Journal of Mathematical and Statistical Psychology}
+#' Ma, W., & de la Torre, J. (2020). An empirical Q-matrix validation method for the sequential G-DINA model. \emph{British Journal of Mathematical and Statistical Psychology, 73}, 142-163.
 #'
 #' Najera, P., Sorrel, M., & Abad, P. (2019). Reconsidering Cutoff Points in the General Method of Empirical Q-Matrix Validation. \emph{Educational and Psychological Measurement}.
 #'
@@ -225,6 +225,7 @@ Qval_wald <- function(GDINA.obj, SE.type = 2,
         index <- data.frame(Cat=rep(1:length(rowSums(Qr) ),2^rowSums(Qr)) )
         index$Column <- seq_len(length(index$Cat))
         sco <- scofun
+
         sco[[j]] <- score_pj(Xj = dat[,j],                   # a vector of item responses to item j
                              parloc.j=etas[j,,drop=FALSE],         # parameter locations for item j - H by 2^K matrix
                              catprob.j=itemparj[j],        # a list with H elements giving the reduced catprob.parm for each nonzero category
